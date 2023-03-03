15b476680da5f4fe347f6315f074094348207297
abhiyelam <abhinandan.yelam@gmail.com>
2023-03-03 09:41:27 +0530

    Add project files.

---
+++ PartialViewDemo/Controllers/EmpoyeeController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PartialViewDemo.Data;
+
+namespace PartialViewDemo.Controllers
+{
+    public class EmpoyeeController : Controller
+    {
+        private readonly ApplicationDbContext db;
+        public EmpoyeeController(ApplicationDbContext db) 
+        { 
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            var model=db.Employees.ToList();
+            return View(model);
+        }
+    }
+}
---
+++ PartialViewDemo/Models/Product.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PartialViewDemo.Models
+{
+    [Table("tblproduct")]
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public string Company { get; set; }
+
+    }
+}
